23129b8c66c9fc63f513878a3eae14e9c91ca2ca
Zalán <zalan.gal.99@gmail.com>
2021-02-26 18:25:33 +0100

    Aktuális állapot

---
+++ main.js
@@ -0,0 +1,14 @@
+function LoadPage(){
+    const app = document.getElementById('app');
+    app.textContent = ConvertToCharacters("Hello World!");
+}
+
+function ConvertToCharacters(str){
+    const lst = [];
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        //Ez még folyamatban van.
+    }
+    return lst;
+}

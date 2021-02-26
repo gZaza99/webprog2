38d0d45d81a0c6e13a824cb1d2c173aefc0c7e28
Zalán <zalan.gal.99@gmail.com>
2021-02-26 18:37:25 +0100

    aktuális állapot

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function LoadPage(){
     const app = document.getElementById('app');
-    app.textContent = ConvertToCharacters("Hello World!");
+    let text = "Hello World!";
+    ConvertToCharacters(text);
+    app.textContent = text;
 }
 
 function ConvertToCharacters(str){
-    const lst = [];
-
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        //Ez még folyamatban van.
+    if (typeof str === 'string') {
+        for (let index = 0; index < str.length; index++) {
+            console.log(str[index]);
+        }
     }
-    return lst;
 }
